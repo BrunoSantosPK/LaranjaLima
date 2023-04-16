dba037820a8f44321a38acd25a8b31dc4b9eceae
unknown <bruno.ajsch@yahoo.com>
2023-04-16 19:06:29 -0300

    inicio da analise com orange

--- src/bombas.py
+++ src/bombas.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 from typing import Tuple
@@ -41,21 +42,26 @@ class PumpParams:
 
 class Pump:
 
-    def __init__(self, pressure: float, temperature: float, flow: float, spin: float, params: PumpParams) -> None:
+    def __init__(self, pressure: float, temperature: float, flow: float, spin: float, params: PumpParams, name: str) -> None:
         self.__max_pressure = pressure
         self.__max_temperature = temperature
         self.__max_flow = flow
         self.__max_spin = spin
         self.__params = params
+        self.__name = name
         
         self.__jobs = []
         self.__total_work = 0
         self.__work_before_maintenance = 0
 
+    def get_work_time(self) -> float:
+        return self.__work_before_maintenance
+
     def operation(self, date: datetime, preventive=False) -> None:
         # Obtêm parâmetros iniciais da operação
         registry = self.__params.get_operation()
         registry["Data Execução"] = date.isoformat()
+        registry["Local"] = self.__name
 
         # Realiza manutenção preventiva, caso informado
         if preventive: self.maintenance()
@@ -97,23 +103,33 @@ class Pump:
         self.__work_before_maintenance = 0
 
     def break_analysis(self) -> bool:
-        beta, hours = 250, self.__work_before_maintenance
-        prob = 0.05 if hours < 100 else np.exp(hours / beta) / (np.exp(hours / beta) + 1)
+        beta, hours = 20, self.__work_before_maintenance
+        prob = 0.05 if hours < 10 else np.exp(hours / beta) / (np.exp(hours / beta) + 1)
         sort = np.random.random_sample()
         return True if sort < prob else False
 
 
 if __name__ == "__main__":
     # Bomba na área de descarga, transfere o leite do caminhão para os tanques
-    params_01 = PumpParams(20, 2, 80, 500, 0.5, 2.2)
+    params_01 = PumpParams(20, 2, 80, 500, 1, 2.2)
     params_01.set_range((-2, 1), (-0.5, 0.5), (-10, 60), (-50, 200), (-0.2, 0.6), (-0.5, 0.3))
-    pump_01 = Pump(10, 120, 180, 1800, params_01)
+    pump_01 = Pump(10, 120, 180, 1800, params_01, "Bomba da Entrada")
 
     # Bombas dos tanques de armazenamento para o misturador
+    params_02 = PumpParams(18, 1, 50, 800, 0.8, 2.0)
+    params_02.set_range((-1, 4), (-0.1, 0.3), (-10, 60), (-50, 50), (-0.1, 0.3), (-0.1, 0.6))
+    pump_02 = Pump(10, 120, 180, 1800, params_02, "Bomba do Misturador")
 
     # Bomba para transporte de leite no trocador de calor de aquecimento
+    # temperatura, pressão, vazão, rotação, tempo e viscosidade
+    params_03 = PumpParams(21, 1, 80, 700, 0.1, 2.2)
+    params_03.set_range((-1, 2), (0, 0.1), (-10, 20), (-50, 50), (0, 0.1), (-0.1, 0.1))
+    pump_03 = Pump(10, 120, 180, 1800, params_03, "Bomba de Leite do Trocador de Calor")
 
     # Bomba para transporte de água no trocador de calor de aquecimento
+    params_04 = PumpParams(50, 1.5, 30, 1000, 0.1, 1)
+    params_04.set_range((-2, 5), (0, 0.6), (-5, 10), (-100, 100), (0, 0.1), (-0.1, 0.2))
+    pump_04 = Pump(3, 90, 40, 3500, params_04, "Bomba de Água do Trocador de Calor")
 
     # Bomba para transporte de leite no trocador de calor de resfriamento
 
@@ -125,21 +141,29 @@ if __name__ == "__main__":
     start_date = datetime(2022, 1, 1, 8, 0, 0)
     end_date = datetime(2023, 4, 30, 18, 0, 0)
     current_date = start_date
-    continuous = 0
 
     while current_date < end_date:
-        # Verifica se será executa uma manutenção preventiva
-        preventive = False
-        if continuous >= 336:
-            preventive = True
-            continuous = 0
+        # Caso serja no período noturno, não funcinam as bombas
+        if current_date.hour >= 18:
+            current_date = current_date + timedelta(hours=2)
+            continue
 
         # Simula operação
-        pump_01.operation(current_date, preventive=preventive)
+        preventive_limit = 15
+        pump_01.operation(current_date, preventive=(True if pump_01.get_work_time() > preventive_limit else False))
+        pump_02.operation(current_date, preventive=(True if pump_02.get_work_time() > preventive_limit else False))
+        pump_03.operation(current_date, preventive=(True if pump_03.get_work_time() > preventive_limit else False))
+        pump_04.operation(current_date, preventive=(True if pump_04.get_work_time() > preventive_limit else False))
 
         # Avança no tempo
         current_date = current_date + timedelta(hours=2)
-        continuous = continuous + 2
 
     # Recupera dados
-    pump_01.get_data().to_csv("data/simulacao.csv", index=False, sep=";")
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    with pd.ExcelWriter(f"{base_path}/data/Laticínio.xlsx", mode="a", if_sheet_exists="replace") as file:
+        pd.concat([
+            pump_01.get_data(),
+            pump_02.get_data(),
+            pump_03.get_data(),
+            pump_04.get_data()
+        ]).to_excel(file, sheet_name="Bombas", index=False)
